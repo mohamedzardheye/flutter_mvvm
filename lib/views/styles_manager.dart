d0585e38abfc982c8a2adb79eb6b9e19917d54bc
“mohamedzardheye” <mohamedzardheye@gmail.com>
2023-01-31 20:56:53 +0300

    second git

---
+++ lib/views/styles_manager.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'font_manager.dart';
+
+TextStyle getTextStyle(double fontSize, String fontFamily, Color color, FontWeight fontWeight){
+  return TextStyle(
+    fontSize: fontSize, 
+    fontFamily: fontFamily,
+    color: color,
+    fontWeight: fontWeight);
+}
+
+
+
+// regular style 
+TextStyle getRegularStyle({double fontSize = FontSize.s12, required Color color})
+{
+  return getTextStyle(fontSize, FontConstants.fontFamily, color ,FontWeightManager.regular);
+}
+
+// light style 
+TextStyle getLightStyle({double fontSize = FontSize.s12, required Color color})
+{
+  return getTextStyle(fontSize, FontConstants.fontFamily, color ,FontWeightManager.light);
+}
+
+
+// Medium Style
+TextStyle getMediumStyle({double fontSize = FontSize.s12, required Color color})
+{
+  return getTextStyle(fontSize, FontConstants.fontFamily, color ,FontWeightManager.medium);
+}
+
+// Bold Style
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color})
+{
+  return getTextStyle(fontSize, FontConstants.fontFamily, color ,FontWeightManager.bold);
+}
+
+
+// Semi Bold Style
+TextStyle getSemiBoldStyle({double fontSize = FontSize.s12, required Color color})
+{
+  return getTextStyle(fontSize, FontConstants.fontFamily, color ,FontWeightManager.semiBold);
+}
---
+++ lib/views/values_manager.dart
@@ -0,0 +1,34 @@
+class appMargin{
+  static const double m8 = 8.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+}
+
+
+
+
+class appPadding{
+  static const double m8 = 8.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+}
+
+
+class appSize{
+  static const double m8 = 8.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+  static const double m28 = 28.0;
+  static const double m60 = 60.0;
+  static const double m65 = 65.0;
+
+}
