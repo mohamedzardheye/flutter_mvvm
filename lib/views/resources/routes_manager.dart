493f3db8c6fec714ab74e91f321d78eab598cf05
“mohamedzardheye” <mohamedzardheye@gmail.com>
2023-07-19 11:07:06 +0300

    added

--- lib/views/resources/routes_manager.dart
+++ lib/views/resources/routes_manager.dart
@@ -6,6 +6,7 @@ import '../onboarding/onboarding.dart';
 import '../register.dart/register.dart';
 import '../store_details/store_details.dart';
 import '../splash/splash.dart';
+import 'strings_manager.dart';
 
 class Routes{
   static const String splashRoute = "/";
@@ -58,8 +59,8 @@ class RouteGenerator{
   static Route<dynamic> unDefinedRoute(){
     return MaterialPageRoute(builder: (_) => 
     Scaffold(
-      appBar: AppBar(title: const Text("No Route Found"),),
-      body: const Center(child: Text("No Route Found")),
+      appBar: AppBar(title: const Text(AppStrings.noRouteFound),),
+      body: const Center(child: Text(AppStrings.noRouteFound)),
     )
     );
   }
