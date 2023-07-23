faeba2066e84e6657fe94736b1f59e61ca01930b
“mohamedzardheye” <mohamedzardheye@gmail.com>
2023-07-23 12:12:34 +0300

    splash

--- lib/views/resources/strings_manager.dart
+++ lib/views/resources/strings_manager.dart
@@ -1,4 +1,16 @@
 class AppStrings{
   static const noRouteFound = "No Route Found";
 
+
+  static const onBoardingTitle1 = "Travel the easiest way";
+  static const onBoardingTitle2 = "Travel the easiest way";
+  static const onBoardingTitle3 = "Travel the easiest way";
+  static const onBoardingTitle4 = "Travel the easiest way";
+
+  static const String onBoardingSubTitle1 = "sahal transport v2 welcome to sahal";
+  static const String onBoardingSubTitle2 = "sahal transport v2 welcome to sahal";
+  static const String onBoardingSubTitle3 = "sahal transport v2 welcome to sahal";
+  static const String onBoardingSubTitle4 = "sahal transport v2 welcome to sahal";
+
+
 }
